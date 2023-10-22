5349709d416c4028a97cea093c85cf5e9d8da8f4
Konrad <flansmaster@163.com>
2023-10-22 11:24:44 +0800

    add icon

--- ClickStimulator.user.js
+++ ClickStimulator.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         Click Stimulator
-// @namespace    http://tampermonkey.net/
+// @namespace    https://github.com/MegaSuite/HUST-Network-Toolkit
 // @version      0.1
 // @description  stimulate clicking login button in the login page of HUST Network
 // @author       Konrad Gerrens
 // @match        http://192.168.50.3:8080/*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=csdn.net
+// @icon         https://github.com/MegaSuite/HUST-Network-Toolkit/blob/main/HUST-logo.png?raw=true
 // ==/UserScript==
 
 (function() {
